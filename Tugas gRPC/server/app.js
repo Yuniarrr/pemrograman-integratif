0337c292e43f3495838422c1d19d35de7936baf6
Yuniarrr <5027211025@mhs.its.ac.id>
2023-03-30 23:56:33 +0700

    feat: add server
    feat: add frontend

--- Tugas gRPC/server/app.js
+++ Tugas gRPC/server/app.js
@@ -1,8 +1,8 @@
 // Import package 
 const grpc = require('@grpc/grpc-js');
 const protoLoader = require('@grpc/proto-loader');
-const MongoClient = require('mongodb').MongoClient;
 const api = require('./db.js');
+const mongoose = require('mongoose');
 
 // Define Proto path 
 const PROTO_PATH = './mahasiswa.proto';
@@ -20,97 +20,95 @@ const packageDefinition = protoLoader.loadSync(PROTO_PATH, options);
 // Load Proto 
 const mahasiswaProto = grpc.loadPackageDefinition(packageDefinition);
 
-const server = new grpc.Server();
-
 // Connection URL and database name
-const url = 'mongodb://admin:pass12345@localhost:27017/?authSource=admin';
+const url = 'mongodb://admin:pass12345@localhost:27017/mahasiswa?authSource=admin';
 const dbName = 'mahasiswa';
 
-const dbClient = new MongoClient(url, { useUnifiedTopology: true });
-let db;
+const Mahasiswa = mongoose.model('mahasiswa', new mongoose.Schema({
+    id: String,
+    nama: String,
+    nrp: String,
+    nilai: String
+}));
+
 
 async function connectDB() {
     try {
-        await dbClient.connect();
-        db = await dbClient.db(dbName);
-        db.command({ ping: 1 });
-        console.log("Connected successfully to mongo server");
-        // Create index
-        await db.collection("mahasiswa").createIndex({ email: 1 });
+        const connect = await mongoose.connect(url);
+        if (connect) {
+            console.log("Connected successfully to mongo server");
+        }
     } catch (e) {
         console.error(e);
     }
 }
 
 async function main() {
-    await connectDB().catch(console.dir);
+    // await connectDB().catch(console.dir);
+    await connectDB().catch(err => console.log(err));
     let server = new grpc.Server();
 
     server.addService(mahasiswaProto.MahasiswaService.service, {
         getAll: async (_, callback) => {
-            db.collection('mahasiswa').find().toArray((err, result) => {
-                if (err) {
-                    console.error(err);
-                    callback(err, null);
-                } else {
-                    const mahasiswa = { mahasiswa: result };
-                    callback(null, mahasiswa);
-                }
-            });
+            try {
+                const mahasiswa = await Mahasiswa.find();
+                callback(null, { mahasiswa });
+            } catch (error) {
+                callback(error, null);
+            }
         },
         deleteMahasiswa: async (call, callback) => {
             const _id = call.request.id;
-            db.collection('mahasiswa').deleteOne({ _id: ObjectId(_id) }, (err, result) => {
-                if (err) {
-                    console.error(err);
-                    callback(err, null);
-                } else if (result.deletedCount === 0) {
+            try {
+                const mahasiswa = await Mahasiswa.findByIdAndDelete(_id);
+                if (!mahasiswa) {
                     callback({ code: grpc.status.NOT_FOUND, message: 'Mahasiswa not found' }, null);
                 } else {
-                    console.log(`${result.deletedCount} document(s) deleted`);
-                    callback(null, { id: _id });
+                    console.log(`1 document deleted`);
+                    callback(null, {});
                 }
-            });
+            } catch (error) {
+                callback(error, null);
+            }
         },
         updateMahasiswa: async (call, callback) => {
             const _id = call.request.id;
             const _mahasiswa = call.request;
-            db.collection('mahasiswa').updateOne({ _id: ObjectId(_id) }, { $set: _mahasiswa }, (err, result) => {
-                if (err) {
-                    console.error(err);
-                    callback(err, null);
-                } else if (result.matchedCount === 0) {
+            try {
+                const mahasiswa = await Mahasiswa.findByIdAndUpdate(_mahasiswa);
+                if (!mahasiswa) {
                     callback({ code: grpc.status.NOT_FOUND, message: 'Mahasiswa not found' }, null);
                 } else {
-                    console.log(`${result.modifiedCount} document(s) updated`);
+                    console.log(`1 document updated`);
                     callback(null, _mahasiswa);
                 }
-            });
+            } catch (error) {
+                callback(error, null);
+            }
         },
         getMahasiswa: async (call, callback) => {
             const _id = call.request.id;
-            db.collection('mahasiswa').findOne({ _id: ObjectId(_id) }, (err, result) => {
-                if (err) {
-                    console.error(err);
-                    callback(err, null);
-                } else if (result === null) {
+            try {
+                const mahasiswa = await Mahasiswa.findById(_id);
+                if (!mahasiswa) {
                     callback({ code: grpc.status.NOT_FOUND, message: 'Mahasiswa not found' }, null);
                 } else {
-                    callback(null, result);
+                    callback(null, mahasiswa);
                 }
-            });
+            } catch (error) {
+                callback(error, null);
+            }
         },
         addMahasiswa: async (call, callback) => {
             const _mahasiswa = call.request;
-            db.collection('mahasiswa').insertOne(_mahasiswa, (err, result) => {
-                if (err) {
-                    console.error(err);
-                    callback(err, null);
-                } else {
-                    console.log("1 document inserted");
+            try {
+                const mahasiswa = new Mahasiswa(_mahasiswa);
+                if (await mahasiswa.save()) {
                     callback(null, _mahasiswa);
                 }
-            });
+            } catch (error) {
+                callback(error, null);
+            }
         },
         createDatabase: (_, callback) => {
             db.createCollection("mahasiswa", (err, res) => {
